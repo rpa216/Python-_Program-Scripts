c88bd5e32780bd5467f4ca74746a8e64739256f1
rpa216 <raj55.patel@gmail.com>
2018-01-27 08:58:32 -0500

    Update Paranthesis checker.py

--- Paranthesis checker.py
+++ Paranthesis checker.py
@@ -1,25 +1,41 @@
 #Stack Implementation to check the balance of paranthesis
-def parChecker(symbolstring):
-	s= Stack()
-	balanced = True
-	index = 0
-	while index < len(symbolstring) and balanced:
-		symbol = symbolstring[index]
-		if symbol == '(':
-			s.push(symbol)
-		else:
-			if s.isEmpty():
-				balanced = False
-			else:
-				top = s.pop()
-				if not matches(top, symbol):
-					balanced = False
-		index = index +1
+#!/bin/python3
 
-	if balanced and s.isEmpty():
-		return(True)
-	else:
-		return(False)
+import sys
 
-print(parChecker('((()))'))
-print(parChecker('(()'))
+def isBalanced(symbolstring):
+    s = []
+    balanced =True
+    index = 0
+    while index < len(symbolstring) and balanced:
+        symbol = symbolstring[index]
+        if symbol in "([{":
+            s.append(symbol)
+            #print(symbol)
+        else:
+            if len(s) == 0:
+                balanced = False
+            else:
+                top = s.pop()
+                #print(top)
+                if not matches(top, symbol):
+                    balanced = False
+        index = index+1
+    if balanced and len(s)==0:
+        return("YES")
+    else:
+        return("NO")
+def matches(a,b):
+    opens = "([{"
+    closers = ")]}"
+    if opens.index(a) == closers.index(b):
+        return(True)
+    else:
+        return(False)
+
+if __name__ == "__main__":
+    t = int(input().strip())
+    for a0 in range(t):
+        s = input().strip()
+        result = isBalanced(s)
+        print(result)
